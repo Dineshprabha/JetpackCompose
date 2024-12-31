b3aa4f69f7cea65e4223ae683ad8be12f870fdda
Dinesh N <dinesh.prabha1205@gmail.com>
2024-12-31 08:19:37 +0530

    Add paint draw functionality using canvas

--- app/src/main/java/com/dinesh/jetpackcompose/painting/DrawScreen.kt
+++ app/src/main/java/com/dinesh/jetpackcompose/painting/DrawScreen.kt
@@ -1,6 +1,9 @@
 package com.dinesh.jetpackcompose.painting
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
@@ -11,15 +14,29 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.dinesh.jetpackcompose.painting.model.Line
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DrawScreen() {
 
+    val lines = remember {
+        mutableStateListOf<Line>()
+    }
+    var currentColor by remember {
+        mutableStateOf(Color.Black)
+    }
     Column {
 
         TopAppBar(
@@ -33,7 +50,7 @@ fun DrawScreen() {
             actions = {
                 Button(
                     colors = ButtonDefaults.buttonColors(Color.Black),
-                    onClick = { /*TODO*/ },
+                    onClick = { currentColor = Color.Black },
                     modifier = Modifier
                         .padding(3.dp)
                         .width(40.dp)
@@ -42,7 +59,7 @@ fun DrawScreen() {
                 }
                 Button(
                     colors = ButtonDefaults.buttonColors(Color.Red),
-                    onClick = { /*TODO*/ },
+                    onClick = { currentColor = Color.Red },
                     modifier = Modifier
                         .padding(3.dp)
                         .width(40.dp)
@@ -52,7 +69,7 @@ fun DrawScreen() {
 
                 Button(
                     colors = ButtonDefaults.buttonColors(Color.Green),
-                    onClick = { /*TODO*/ },
+                    onClick = { currentColor = Color.Green },
                     modifier = Modifier
                         .padding(3.dp)
                         .width(40.dp)
@@ -62,7 +79,7 @@ fun DrawScreen() {
 
                 Button(
                     colors = ButtonDefaults.buttonColors(Color.Blue),
-                    onClick = { /*TODO*/ },
+                    onClick = { currentColor = Color.Blue },
                     modifier = Modifier
                         .padding(3.dp)
                         .width(40.dp)
@@ -72,7 +89,7 @@ fun DrawScreen() {
 
                 Button(
                     colors = ButtonDefaults.buttonColors(Color.LightGray),
-                    onClick = { /*TODO*/ },
+                    onClick = { lines.clear() },
                     modifier = Modifier
                         .padding(3.dp)
 
@@ -82,5 +99,32 @@ fun DrawScreen() {
 
             }
         )
+        
+        Canvas(modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(true) {
+                detectDragGestures { change, dragAmount ->
+                    change.consume()
+
+                    val line = Line(
+                        start = change.position - dragAmount,
+                        end = change.position,
+                        color = currentColor
+                    )
+                    lines.add(line)
+                }
+            }) {
+
+            lines.forEach { line ->
+                drawLine(
+                    color = line.color,
+                    start = line.start,
+                    end = line.end,
+                    strokeWidth = line.strokeWidth,
+                    cap = StrokeCap.Round
+                )
+
+            }
+        }
     }
 }
